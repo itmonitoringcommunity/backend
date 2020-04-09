6771a8fd4e5ca9db798e78065c9a1585e16f4177
itmonitoringcommunity <itmonitoringcommunity@gmail.com>
2020-04-09 19:46:55 +0300

    Commit message

--- djreact/settings.py
+++ djreact/settings.py
@@ -62,10 +62,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djreact.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'django_db',
+        'HOST': 'DESKTOP-Q9HJHBN',
+        'USER': 'django_user',
+        'PASSWORD': '123456',
+
+        'OPTIONS': {
+            'driver': 'SQL Server Native Client 11.0',
+        }
     }
 }
 
